cf52795bad47541a0c8b034ebc92160bc47c88ce
Cristian David Pinto Penaloza <cristiandavid.pinto@globant.com>
2020-04-18 10:37:26 -0500

    added productosMock

--- public/js/productos.js
+++ public/js/productos.js
@@ -2,23 +2,61 @@ const nombreProducto = document.getElementById("nombreProducto");
 const descripcionProducto = document.getElementById("descripcionProducto");
 const crearProducto = document.getElementById("crearProducto");
 
-crearProducto.addEventListener("click", function() {
-    var producto = { nombre: descripcionProducto.value, descripcion: descripcionProducto.value, disponible: true };
-    crearProducto(crearProducto)
-})
+const productosMock = [
+  {
+    id: 1,
+    nombre: "Iphone 8",
+    descripcion: "Espectacular Iphone 8",
+    disponible: true,
+    comprador: null,
+    vendedor: "cris18",
+  },
+  {
+    id: 2,
+    nombre: "Iphone 7",
+    descripcion: "Espectacular Iphone 7",
+    disponible: true,
+    comprador: null,
+    vendedor: "cris18",
+  },
+  {
+    id: 3,
+    nombre: "Iphone 6",
+    descripcion: "Espectacular Iphone 6",
+    disponible: true,
+    comprador: null,
+    vendedor: "cris18",
+  },
+  {
+    id: 4,
+    nombre: "Iphone 5",
+    descripcion: "Espectacular Iphone 5",
+    disponible: true,
+    comprador: null,
+    vendedor: "cris18",
+  },
+];
 
-function crearProducto() {
-    fetch("http://localhost:3001/productos", {
-            method: "post",
-            body: JSON.stringify(producto),
-            headers: {
-                "Content-Type": "application/json",
-            },
-        })
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            console.log(data);
-        });
+crearProducto.addEventListener("click", function () {
+  var producto = {
+    nombre: nombreProducto.value,
+    descripcion: descripcionProducto.value,
+  };
+  enviarProducto(producto);
+});
+
+function enviarProducto(producto) {
+  fetch("http://localhost:3001/productos", {
+    method: "post",
+    body: JSON.stringify(producto),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
 }
